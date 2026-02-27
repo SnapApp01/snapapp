b2a869c1135f90a3d43ed4406bdd0139f7d57dc9
TIMOTHYBABA2023 <timothybaba01@gmail.com>
2026-02-27 12:49:54 +0100

    FirebaseConfig update...

---
+++ src/main/java/com/snappapp/snapng/config/FirebaseConfig.java
@@ -0,0 +1,35 @@
+package com.snappapp.snapng.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PostConstruct;
+import java.io.FileInputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value("${FIREBASE_CONFIG}")
+    private String firebaseConfigPath;
+
+    @PostConstruct
+    public void init() {
+        try (FileInputStream serviceAccount =
+                     new FileInputStream(firebaseConfigPath)) {
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to initialize Firebase", e);
+        }
+    }
+}
